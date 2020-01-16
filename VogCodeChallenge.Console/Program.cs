219d01e436434ce11057609d8589ea5284312dc7
MujieYang <yangmujie@hotmail.com>
2020-01-16 17:46:25 -0400

    question 9 missing file

---
+++ VogCodeChallenge.Console/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace VogCodeChallenge.ConsoleQuestion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            QuestionClass.TestQuestion();
+        }
+    }
+}
