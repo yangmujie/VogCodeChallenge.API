d36dcff1871fe4c67c8889852667534f1658b34e
MujieYang <yangmujie@hotmail.com>
2020-01-16 17:40:11 -0400

    question 8

---
+++ VogCodeChallenge.Console/QuestionClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VogCodeChallenge.ConsoleQuestion
+{
+    public static class QuestionClass
+    {
+        static List<string> NamesList = new List<string>()
+        {
+            "Jimmy",
+            "jeffrey"
+        };
+
+        public static void TestQuestion()
+        {
+            //using (var enumerator = NamesList.GetEnumerator())
+            //{
+            //    while (enumerator.MoveNext())
+            //    {
+            //        Console.WriteLine(enumerator.Current);
+            //    }
+            //}
+
+            //easier way
+            foreach (var item in NamesList)
+            {
+                Console.WriteLine(item);
+            }
+
+            //faster way
+            for (int i = 0; i < NamesList.Count; i++)
+            {
+                Console.WriteLine(NamesList[i]);
+            }
+        }
+    }
+
+}
